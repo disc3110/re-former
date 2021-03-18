fa413a9040e8a463c8b4d3eb9b59e01fea5fd45b
kimothokamau <kimothokamau@gmail.com>
2021-03-18 13:31:37 -0500

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,40 @@
 class UsersController < ApplicationController
-    def index
-        @users = User.all
-      end
-      
-      def new
-        @user = User.new
-      end
-    
-      def create
-        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-        @user = User.new(user_params)
+  def index
+    @users = User.all
+  end
 
-        if @user.save
-          redirect_to new_user_path
-        else
-          render :new
-        end       
-      end
+  def new
+    @user = User.new
+  end
 
-      def edit
-        @user = User.find(params[:id])
-      end
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
 
-      def update
-        @user = User.find(params[:id])
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+  end
 
-        if @user.update(user_params)
-            redirect_to @user
-        else
-            render :edit
-        end
-      end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-      private
-      def user_params
-        params.require(:user).permit(:username, :email, :password)
-      end
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
